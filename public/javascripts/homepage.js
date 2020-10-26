0a312adc438ff42952595fb86a9f1412085bdbb3
ses7578 <ses7578@g.rit.edu>
2020-10-26 07:39:16 -0400

    Working on the home/mod page and hooking a few things up

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,9 +1,34 @@
 
-let submitButton = document.getElementById("click");
+let upvoteButton = document.getElementById("upvote");
+let downvoteButton = document.getElementById("downvote");
 
-let validate = function(){
-    document.getElementById("hello").innerText = "Button Has Been Clicked"
+let upvote = function ()
+{
+    karmaValue = parseInt(document.getElementById("karma").innerText)
+    document.getElementById("karma").innerText = karmaValue + 1;
 }
 
-submitButton.onclick = function () { validate() };
+let downvote = function(){
+    karmaValue = parseInt(document.getElementById("karma").innerText)
+    document.getElementById("karma").innerText = karmaValue - 1;
+}
+
+let upvoteButton2 = document.getElementById("upvote2");
+let downvoteButton2 = document.getElementById("downvote2");
+
+let upvote2 = function ()
+{
+    karmaValue = parseInt(document.getElementById("karma2").innerText)
+    document.getElementById("karma2").innerText = karmaValue + 1;
+}
+
+let downvote2 = function(){
+    karmaValue = parseInt(document.getElementById("karma2").innerText)
+    document.getElementById("karma2").innerText = karmaValue - 1;
+}
+
+upvoteButton.onclick = function () { upvote() };
+downvoteButton.onclick=function() { downvote() };
+upvoteButton2.onclick = function () { upvote2() };
+downvoteButton2.onclick=function() { downvote2() };
 
