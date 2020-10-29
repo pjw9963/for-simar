3a05b39d99d8a0a7980c4cd11b36642126068a5b
Patrick <pjw9963@rit.edu>
2020-10-29 01:23:35 -0400

    added and built database

--- api/app.js
+++ api/app.js
@@ -3,15 +3,114 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { Sequelize, DataTypes } = require('sequelize');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//database setup
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'data/database.sqlite'
+});
+
+const User = sequelize.define('User', {
+  // Model attributes are defined here
+  userName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  passsword: {
+    type: DataTypes.STRING,
+    allowNull: false            //definilty encrypt passwords if this was for real
+  }
+});
+
+const Post = sequelize.define('Post', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  text: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  upVotes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  downVotes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  imageName: {
+    type: DataTypes.STRING,
+    allowNull: false          
+  },
+  imageLocation: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+const Comment = sequelize.define('Comment', {
+  text: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  author: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  upVotes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  downVotes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+});
+
+const Message = sequelize.define('Message', {
+  text: {
+    type: DataTypes.TEXT
+  }
+});
+
+User.hasMany(Post);
+Post.hasOne(User);
+Post.hasMany(Comment);
+Message.belongsTo(User, {as: 'sender'});
+Message.belongsTo(User, {as: 'receiver'});
+
+user_model();
+post_model();
+comment_model();
+message_model();
+
+async function user_model(){
+  await User.sync({ force: true });
+  console.log("The table for the User model was just (re)created!");
+}
+
+async function post_model(){
+  await Post.sync({ force: true });
+  console.log("The table for the Post model was just (re)created!");
+}
+
+async function comment_model(){
+  await Comment.sync({ force: true });
+  console.log("The table for the Comment model was just (re)created!");
+}
+
+async function message_model(){
+  await Message.sync({ force: true });
+  console.log("The table for the Message model was just (re)created!");
+}
 
 app.use(logger('dev'));
 app.use(express.json());
