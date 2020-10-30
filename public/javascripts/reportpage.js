23740b306ade61b0440e87ae617031b7a00a0cef
ses7578 <ses7578@g.rit.edu>
2020-10-29 21:15:52 -0400

    Adding more mock ups in case react stuff isn't completed so we actually have a finished prototype to hand in

--- public/javascripts/reportpage.js
+++ public/javascripts/reportpage.js
@@ -1,5 +1,7 @@
 let approveButton = document.getElementById("approve");
 let disapproveButton = document.getElementById("disapprove");
+let remove=false;
+let form = document.getElementById("back");
 
 let approveRemoval = function () {
     document.getElementById("reportResult").innerText = "Offending post has been removed.";
@@ -7,6 +9,7 @@ let approveRemoval = function () {
 
 let disapproveRemoval = function () {
     document.getElementById("reportResult").innerText = "You have discarded the report.";
+    remove = true;
 }
 
 approveButton.onclick = function () {
@@ -17,3 +20,14 @@ disapproveButton.onclick = function () {
     disapproveRemoval();
 }
 
+let formValidation = function(event){
+    if(remove)
+    {
+        form.action = "reportmock.html";
+    }
+    else {
+        form.action = "homepage.html";
+    }
+}
+
+form.onsubmit = function(){formValidation()};
