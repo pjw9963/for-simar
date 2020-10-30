02b5662bace814c48bc2ff6159d24448cc3fa01c
Patrick <pjw9963@rit.edu>
2020-10-29 21:03:03 -0400

    fixed buttons to match design

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,52 +1,50 @@
-import React, { Component } from 'react';
-import Header from './header';
-import PostWrapper from './postwrapper';
+import React, { Component } from "react";
+import Header from "./header";
+import PostWrapper from "./postwrapper";
 
-import '../css/style.css';
+import "../css/style.css";
 
 class Home extends Component {
-  
-    render() {
+  render() {
     return (
-        <div>
-          <Header />
-          <div class="page">
-                <div id="postPreviews">
-                    <form action="postpreview.html" id="post">
-                        <input type="submit" value="Post" />
-                    </form>
-                    <form action="messagePage.html" id="message">
-                        <input type="submit" value="Message" />
-                    </form>
-                    <form action="modpage.html" id="logIn">
-                        <input type="submit" value="Log In"/>
-                    </form>
-                    <PostWrapper
-                        title = "Cat I found"
-                        desc = "This is an image of a cat that I found the other day and this cat was being super adorable and I just love it so much"
-                        image = "/images/kitty.webp"
-                        karma = "10"
-                        numcomments = "4"
-                        numreplies = "1"
-                    />
-                    <PostWrapper 
-                        title = "Picture of RIT"
-                        desc = "Picture of RIT the other day"
-                        image = "/images/rit.jpg"
-                        karma = "68"
-                        numcomments = "13"
-                        numreplies = "6"
-                    />
-                </div>
-                <div id="sideBar">
-                    this is the left margin
-                </div>
-                <a href="postpage.html" class="btn btn-primary">Click for Post Page</a>
-            </div>
+      <div>
+        <Header />
+        <div class="page">
+          <a class="link-button" href="preview">
+            Post
+          </a>
+          <a class="link-button" href="messages">
+            Messages
+          </a>
+          <a class="link-button" href="mod">
+            Log in
+          </a>
+          <div id="postPreviews">
+            <PostWrapper
+              title="Cat I found"
+              desc="This is an image of a cat that I found the other day and this cat was being super adorable and I just love it so much"
+              image="/images/kitty.webp"
+              karma="10"
+              numcomments="4"
+              numreplies="1"
+            />
+            <PostWrapper
+              title="Picture of RIT"
+              desc="Picture of RIT the other day"
+              image="/images/rit.jpg"
+              karma="68"
+              numcomments="13"
+              numreplies="6"
+            />
+          </div>
+          <div id="sideBar">this is the left margin</div>
+          <a href="preview" class="btn btn-primary">
+            Click for Post Page
+          </a>
         </div>
+      </div>
     );
   }
-
 }
 
 export default Home;
