e029cada981d0defe1aee26394366c54b00fbebc
Patrick <pjw9963@rit.edu>
2020-10-30 03:19:23 -0400

    made mod page load posts dynamically

--- frontend/src/components/modpage.js
+++ frontend/src/components/modpage.js
@@ -1,60 +1,59 @@
-import React, { Component } from 'react';
-import Header from './header';
-import Post from './post';
+import React, { Component } from "react";
+import Header from "./header";
+import PostWrapper from "./postwrapper";
 
-import '../css/style.css';
+import "../css/style.css";
 
 class ModPage extends Component {
-  
-    render() {
-    return (
-        <div>
-          <Header />
-          <div class="page">
-                <div id="postPreviews" style={{color:"white"}}>
-                    <form action="postpreview.html" id="post">
-                        <input type="submit" value="Post" />
-                    </form>
-                    <form action="messagePage.html" id="message">
-                        <input type="submit" value="Message" />
-                    </form>
-                    <form action="modpage.html" id="logIn">
-                        <input type="submit" value="Log In"/>
-                    </form>
-                    <h1>Cat that I found (hardcoded value)</h1>
-                    <Post
-                        title = "Cat I found"
-                        desc = "This is an image of a cat that I found the other day and this cat was being super adorable and I just love it so much"
-                        image = "/images/kitty.webp"
-                        karma = "10"
-                        numcomments = "4"
-                        numreplies = "1"
-                    />
-                    <hr/>
-                    <h1>Picture of RIT (hardcoded value)</h1>
-                    <Post 
-                        title = "Picture of RIT"
-                        desc = "Picture of RIT the other day"
-                        image = "/images/rit.jpg"
-                        karma = "68"
-                        numcomments = "13"
-                        numreplies = "6"
-                        showtitle = {true}
-                    />
-                </div>
-                <div id="sideBar">
-                    this is the trending margin
-                </div>
-                <div id="reportsBar">
-                    this is the report margin
-                    <p></p>
-                    <a href="reportpage.html">I hate Cats</a>
-                </div>
-            </div>
-        </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
   }
 
+  componentDidMount() {
+    var myRequest = new Request("http://localhost:3001/api/posts");
+    let posts = [];
+
+    fetch(myRequest)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ posts: data });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div class="page">
+          <div id="postPreviews">
+            {this.state.posts.map((post) => {
+              return (
+                <div>
+                  <PostWrapper
+                    title={post.title}
+                    desc={post.text}
+                    image={`http://localhost:3001/${post.imageName}`}
+                    karma={post.upVotes + post.downVotes}
+                    numcomments="4"
+                    numreplies="1"
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <div id="sideBar">this is the trending margin</div>
+          <div id="reportsBar">
+            this is the report margin
+            <p></p>
+            <a href="reportpage.html">I hate Cats</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ModPage;
